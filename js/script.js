89c4bfa815d3f76413cdc7120ede3a3939e5d71a
shantadurgaprasanna <shantadurgaprasanna@gmail.com>
2018-06-28 17:21:22 +0530

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -73,4 +73,31 @@ $(function(){
 	  });
 	// Mix gallery Slick carousal code end here
 
+	// Event and Guest Slick carousal
+	$('.guestCarousal, .eventCarousal').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [{
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+           breakpoint: 400,
+           settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+           }
+        }]
+    });
+
 });
