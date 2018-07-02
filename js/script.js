8e6f64b792f76680d6d65c752c5836ea6562428c
shantadurgaprasanna <shantadurgaprasanna@gmail.com>
2018-07-02 19:02:45 +0530

    Changes commit

--- js/script.js
+++ js/script.js
@@ -101,6 +101,10 @@ $(function(){
         }]
     });
     // Event and Guest Slick carousal Code End Here
+
+    // Map code goes here
+    google.maps.event.addDomListener(window, 'load', init);
+    // Map code end here
  });
 
 // $(document).ready(function(){
@@ -112,3 +116,225 @@ $(function(){
 
 //      // $("#videoGallery").lightGallery();
 // });
+
+
+// Map code goes here
+function init() {
+   // Basic options for a simple Google Map
+   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+   var mapOptions = {
+     // How zoomed in you want the map to start at (always required)
+     zoom: 11,
+
+     // The latitude and longitude to center the map (always required)
+     center: new google.maps.LatLng(40.6700, -73.9400), // New York
+
+     // How you would like to style the map. 
+     // This is where you would paste any style found on Snazzy Maps.
+     styles: [{
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ecddbb"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#d9d8b2"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#e1d0ad"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#9f8e7a"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#7d7971"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#f3e0c1"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#d5c2a3"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#46bcec"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#a5d2c9"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#838383"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }]
+   };
+
+   // Get the HTML DOM element that will contain your map 
+   // We are using a div with id="map" seen below in the <body>
+   var mapElement = document.getElementById('map');
+
+   // Create the Google Map using our element and options defined above
+   var map = new google.maps.Map(mapElement, mapOptions);
+
+   // Let's also add a marker while we're at it
+   var marker = new google.maps.Marker({
+     position: new google.maps.LatLng(40.6700, -73.9400),
+     map: map,
+     title: 'Snazzy!'
+   });
+ }
+
+ // Map code goes here
